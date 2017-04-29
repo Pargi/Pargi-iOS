9833f8ef6f6ac09bde4042521df62bc984de979a
Henri Normak <henri.normak@gmail.com>
2017-04-29 13:45:38 +0300

    Initial implementation of zone buttons in detail view
    The buttons will be the main interaction point for quickly picking a zone based on what is visible on the map.

--- Pargi/DetailViewController.swift
+++ Pargi/DetailViewController.swift
@@ -16,6 +16,73 @@ import Pulley
 
 class DetailViewController: UIViewController, PulleyDrawerViewControllerDelegate {
     
+    @IBOutlet var zoneStackView: UIStackView!
+    
+    var delegate: DetailViewControllerDelegate? = nil
+    
+    var selectedZone: Zone? = nil {
+        didSet {
+            // Modify buttons so that the selected one stands out
+            if let zone = self.selectedZone, let index = self.zones.index(of: zone) {
+                for (idx, view) in self.zoneStackView.arrangedSubviews.enumerated() {
+                    guard let btn = view as? UIButton else {
+                        continue
+                    }
+                    
+                    btn.alpha = index == idx ? 1.0 : 0.4
+                }
+            } else {
+                // No zone selected
+                self.zoneStackView.arrangedSubviews.forEach({ view in
+                    guard let btn = view as? UIButton else {
+                        return
+                    }
+                    
+                    btn.alpha = 0.4
+                })
+            }
+            
+            self.delegate?.detailViewController(self, didSelectZone: self.selectedZone)
+        }
+    }
+    
+    // List of zones to allow the user to choose from, about 3 or so will suffice
+    // (limit will be enforced by not displaying more than we have buttons available)
+    var zones: [Zone] = [] {
+        didSet {
+            // Update up-to n buttons (as many as we have in the stack view)
+            for (idx, view) in self.zoneStackView.arrangedSubviews.enumerated() {
+                guard let btn = view as? UIButton else {
+                    continue
+                }
+                
+                if self.zones.count <= idx {
+                    btn.isHidden = true
+                    continue
+                }
+                
+                let zone = self.zones[idx]
+                let provider = ApplicationData.currentDatabase.provider(for: zone)
+                btn.backgroundColor = provider?.color
+                btn.setTitle(zone.code, for: .normal)
+                btn.isHidden = false
+            }
+            
+            // Default selected zone to the first of the new zones
+            self.selectedZone = self.zones.first
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func tapped(zoneButton: UIButton) {
+        guard let idx = (zoneButton.superview as? UIStackView)?.arrangedSubviews.index(of: zoneButton) else {
+            return
+        }
+        
+        self.selectedZone = self.zones[idx]
+    }
+    
     // MARK: PulleyDrawerViewControllerDelegate
     
     func collapsedDrawerHeight() -> CGFloat {
@@ -29,4 +96,8 @@ class DetailViewController: UIViewController, PulleyDrawerViewControllerDelegate
     func supportedDrawerPositions() -> [PulleyPosition] {
         return [.collapsed, .partiallyRevealed]
     }
+}
+
+protocol DetailViewControllerDelegate {
+    func detailViewController(_ controller: DetailViewController, didSelectZone zone: Zone?)
 }
