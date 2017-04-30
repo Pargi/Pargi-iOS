9822f2f40855e3e825c33d75d3e989d86abc211a
Henri Normak <henri.normak@gmail.com>
2017-04-30 14:56:51 +0300

    Add an extension for arrays for shifting

--- Pargi/UI/FoundationExtensions.swift
+++ Pargi/UI/FoundationExtensions.swift
@@ -13,3 +13,18 @@ extension String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: withComment)
     }
 }
+
+extension Array {
+    func shift(withDistance distance: Int = 1) -> Array<Element> {
+        let offsetIndex = distance >= 0 ?
+            self.index(startIndex, offsetBy: distance, limitedBy: endIndex) :
+            self.index(endIndex, offsetBy: distance, limitedBy: startIndex)
+        
+        guard let index = offsetIndex else { return self }
+        return Array(self[index ..< endIndex] + self[startIndex ..< index])
+    }
+    
+    mutating func shiftInPlace(withDistance distance: Int = 1) {
+        self = shift(withDistance: distance)
+    }
+}
