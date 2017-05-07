61343e643151744d2b2297956f9f68dbc2159604
Henri Normak <henri.normak@gmail.com>
2017-05-07 18:26:45 +0300

    Add shortcuts for previous license plates to the input view
    Also add a title to the input view

--- Pargi/CarLicenseViewController.swift
+++ Pargi/CarLicenseViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class CarLicenseViewController: UIViewController {
-    @IBOutlet var textField: UITextField!
+    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var textFieldInputView: UIView!
+    
+    @IBOutlet private var shortcutLicensePlatesStackView: UIStackView!
     
     var licensePlateNumber: String? {
         didSet {
@@ -17,14 +20,45 @@ class CarLicenseViewController: UIViewController {
         }
     }
     
+    var shortcutLicensePlateNumbers: [String] = [] {
+        didSet {
+            self.updateShortcutLicensePlateNumbers()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.textField.text = self.licensePlateNumber
+        self.textField.inputAccessoryView = self.textFieldInputView
+        
+        self.updateShortcutLicensePlateNumbers()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.textField.becomeFirstResponder()
+    }
+    
+    // MARK: UI Updates
+    
+    private func updateShortcutLicensePlateNumbers() {
+        // Clear old plates
+        if let subviews = self.shortcutLicensePlatesStackView?.subviews {
+            for subview in subviews {
+                subview.removeFromSuperview()
+            }
+        }
+        
+        // Create new ones
+        for number in self.shortcutLicensePlateNumbers {
+            let button = UIButton(type: .system)
+            button.setTitle(number, for: .normal)
+            button.contentEdgeInsets = UIEdgeInsets(top: 0.0, left: 30.0, bottom: 0.0, right: 30.0)
+            button.setContentCompressionResistancePriority(UILayoutPriorityRequired, for: .horizontal)
+            button.addTarget(self, action: #selector(shortcutLicensePlateTapped(sender:)), for: .touchUpInside)
+            
+            self.shortcutLicensePlatesStackView?.addArrangedSubview(button)
+        }
     }
     
     // MARK: Actions
@@ -36,5 +70,12 @@ class CarLicenseViewController: UIViewController {
         
         // Cue rewind segue
         self.performSegue(withIdentifier: "licensePlateNumberChanged", sender: textField)
+    }
+    
+    @objc func shortcutLicensePlateTapped(sender: UIButton) {
+        self.licensePlateNumber = sender.titleLabel?.text
+        
+        // Cue rewind segue
+        self.performSegue(withIdentifier: "licensePlateNumberChanged", sender: sender)
     }
 }
